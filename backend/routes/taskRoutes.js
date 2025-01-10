c7f1c3a59bca32d828914d62a85bdc8d1a52a372
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2025-01-10 23:04:30 +0530

    Update taskRoutes.js

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -9,10 +9,10 @@ const {
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
 
-router.post('/tasks',  createTask); // Create task (authenticated)
-router.get('/tasks',  getAllTasks); // Get all tasks (authenticated)
-router.get('/tasks/:taskId',  getTaskById); // Get task by ID (authenticated)
-router.put('/tasks/:taskId',  updateTask); // Update task (authenticated)
-router.delete('/tasks/:taskId',  deleteTask); // Delete task (authenticated)
+router.post('/tasks',  createTask); 
+router.get('/tasks',  getAllTasks); 
+router.get('/tasks/:taskId',  getTaskById); 
+router.put('/tasks/:taskId',  updateTask); 
+router.delete('/tasks/:taskId',  deleteTask); 
 
 module.exports = router;
